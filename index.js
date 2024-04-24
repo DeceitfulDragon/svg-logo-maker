7688cb480fecd6d89f82e7f44d9d2df0322778e5
CatGettingHitInTheFace <feartherenegade@gmail.com>
2024-04-23 21:06:56 -0500

    rewriting index.js to do the prompting and writing

--- index.js
+++ index.js
@@ -1,4 +1,41 @@
-const {startGeneration} = require('./lib/svgmaker');
+const inquirer = require('inquirer');
+const { buildSVG } = require('./lib/svgbuilder');
+const fs = require('fs');
 
-// Runs the inquirer questions in svgmaker
-startGeneration();
+// using async for all this so I can just make one function here
+async function init() {
+  try {
+    const userInput = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'text',
+        message: 'Enter acronym (up to 3 characters)',
+      },
+      {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter color keyword (or hex value):',
+      },
+      {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape:',
+        choices: ['Circle', 'Square', 'Triangle'],
+      },
+      {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter shape color (or hex value):',
+      },
+    ]);
+
+    const svg = buildSVG(userInput);
+
+    // Write to file and log that it's done
+    fs.writeFileSync('logo.svg', svg);
+    console.log('Generated logo.svg!');
+
+  } catch (error) { console.error('Error:', error); }
+}
+
+init();
